b142a57040cdffeda690ba01fdc85e5ce158741f
nigel <nigel_pickard@yahoo.com>
2013-05-24 17:33:02 -0400

    started adding hibernate libs and project dependencies

---
+++ src/com/npickard/hibernate/util/HibernateUtil.java
@@ -0,0 +1,45 @@
+package com.npickard.hibernate.util;
+
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+	static Logger logger = Logger.getLogger(HibernateUtil.class);
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    static
+    {
+        try
+        {
+            Configuration configuration = new Configuration().configure();
+            
+            //add classes here
+            //configuration.addClass(com.npickard.helloworld.Message.class);
+            //configuration.addClass(com.npickard.hibernate.common.Customer.class);
+            
+            
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        catch (HibernateException he)
+        {
+           logger.error("Error creating Session: " + he);
+            throw new ExceptionInInitializerError(he);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+     // Close caches and connection pools
+     getSessionFactory().close();
+    }
+}
